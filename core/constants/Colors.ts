366aa780ddc90876317ac9214e21d5607c5bc2d4
Jordan Souchez <jsouchez@feelity.fr>
2021-11-15 02:28:45 +0100

    💄 Add color constant

--- core/constants/Colors.ts
+++ core/constants/Colors.ts
@@ -1,19 +1,26 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+export const colorBlue = '#4361ed'
+export const colorDarkBlue = '#090954'
+export const colorDarkerBlue = '#070740'
+export const colorCyan = '#4cc8ef'
+export const colorPink = '#f62584'
+export const colorBlack = '#000'
+export const colorWhite = '#fff'
+export const colorLight = '#f8f8f8'
+export const colorGrey = '#ccc'
 
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
-};
+	light: {
+		text: colorDarkerBlue,
+		background: colorLight,
+		tint: colorBlue,
+		tabIconDefault: colorGrey,
+		tabIconSelected: colorCyan,
+	},
+	dark: {
+		text: colorWhite,
+		background: colorDarkerBlue,
+		tint: colorBlue,
+		tabIconDefault: colorGrey,
+		tabIconSelected: colorCyan,
+	},
+}
