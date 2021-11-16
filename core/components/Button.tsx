3bc17672c4dbef02b8953d3867343d66d4ae4e10
Jordan Souchez <jsouchez@feelity.fr>
2021-11-16 21:38:25 +0100

    💄 Adding variant

--- core/components/Button.tsx
+++ core/components/Button.tsx
@@ -1,18 +1,48 @@
-import { colorBlue, colorWhite } from '@constants/Colors'
+import {
+	colorBlack,
+	colorBlue,
+	colorCyan,
+	colorDarkerBlue,
+	colorLight,
+	colorWhite,
+} from '@constants/Colors'
 import React, { ReactChild } from 'react'
 import {
 	View,
-	Text,
 	TouchableOpacity,
 	StyleSheet,
-	TextProps,
+	StyleProp,
+	ViewStyle,
+	TextStyle,
 } from 'react-native'
+import { FredokaText } from './StyledText'
 
-export default function Button({ varia, children }: { children: ReactChild }) {
+export default function Button({
+	variant = 'blue',
+	after,
+	before,
+	children,
+	buttonStyle,
+	textStyle,
+	onClick,
+}: {
+	variant?: 'blue' | 'black' | 'white' | 'cyan'
+	after?: ReactChild
+	before?: ReactChild
+	children?: ReactChild
+	buttonStyle?: StyleProp<ViewStyle>
+	textStyle?: StyleProp<TextStyle>
+	onClick?: CallableFunction
+}) {
 	return (
-		<TouchableOpacity style={styles.buttonBase}>
-			<View>
-				<Text style={styles.text}>{children}</Text>
+		<TouchableOpacity
+			onPress={() => onClick}
+			style={[styles.buttonBase, variants[variant], buttonStyle]}
+		>
+			<View style={styles.groupBase}>
+				{before}
+				<FredokaText style={[styles.text, textStyle]}>{children}</FredokaText>
+				{after}
 			</View>
 		</TouchableOpacity>
 	)
@@ -21,14 +51,35 @@ export default function Button({ varia, children }: { children: ReactChild }) {
 const styles = StyleSheet.create({
 	buttonBase: {
 		padding: 20,
-		backgroundColor: colorBlue,
 		borderRadius: 10,
 		width: '100%',
+	},
+	groupBase: {
+		flexDirection: 'row',
+		justifyContent: 'center',
 	},
 	text: {
 		textAlign: 'center',
 		fontSize: 17,
 		fontFamily: 'FredokaOne',
+	},
+})
+
+const variants = StyleSheet.create({
+	blue: {
+		backgroundColor: colorBlue,
 		color: colorWhite,
+	},
+	cyan: {
+		backgroundColor: colorCyan,
+		color: colorWhite,
+	},
+	black: {
+		backgroundColor: colorBlack,
+		color: colorWhite,
+	},
+	white: {
+		backgroundColor: colorLight,
+		color: colorDarkerBlue,
 	},
 })
