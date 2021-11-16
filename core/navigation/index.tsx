39b3ca0e863ffc1cf740df1b5cd354b521254e6e
Jordan Souchez <jsouchez@feelity.fr>
2021-11-16 21:39:07 +0100

    🎨 Update navigation

--- core/navigation/index.tsx
+++ core/navigation/index.tsx
@@ -14,7 +14,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import * as React from 'react'
 import { ColorSchemeName, Pressable } from 'react-native'
 
-import Colors from '@constants/Colors'
+import Colors, {
+	colorBlue,
+	colorCyan,
+	colorDarkBlue,
+	colorDarkerBlue,
+	colorLight,
+	colorWhite,
+} from '@constants/Colors'
 import useColorScheme from '@hooks/useColorScheme'
 import ModalScreen from '@screens/ModalScreen'
 import NotFoundScreen from '@screens/NotFoundScreen'
@@ -26,6 +33,20 @@ import {
 	RootTabScreenProps,
 } from '@types'
 import LinkingConfiguration from './LinkingConfiguration'
+import AuthScreen from '@screens/auth/AuthScreen'
+import NameScreen from '@screens/auth/inscription/NameScreen'
+
+const Theme = {
+	dark: true,
+	colors: {
+		primary: colorBlue,
+		background: colorDarkerBlue,
+		card: colorWhite,
+		text: colorWhite,
+		border: colorLight,
+		notification: colorCyan,
+	},
+}
 
 export default function Navigation({
 	colorScheme,
@@ -33,10 +54,7 @@ export default function Navigation({
 	colorScheme: ColorSchemeName
 }) {
 	return (
-		<NavigationContainer
-			linking={LinkingConfiguration}
-			theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-		>
+		<NavigationContainer linking={LinkingConfiguration} theme={Theme}>
 			<RootNavigator />
 		</NavigationContainer>
 	)
@@ -52,6 +70,16 @@ function RootNavigator() {
 	return (
 		<Stack.Navigator>
 			<Stack.Screen
+				name='Auth'
+				component={AuthScreen}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
+				name='Inscription'
+				component={RegisterSteps}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen
 				name='Root'
 				component={BottomTabNavigator}
 				options={{ headerShown: false }}
@@ -64,6 +92,18 @@ function RootNavigator() {
 			<Stack.Group screenOptions={{ presentation: 'modal' }}>
 				<Stack.Screen name='Modal' component={ModalScreen} />
 			</Stack.Group>
+		</Stack.Navigator>
+	)
+}
+
+function RegisterSteps() {
+	return (
+		<Stack.Navigator initialRouteName='Name'>
+			<Stack.Screen
+				name='Name'
+				component={NameScreen}
+				options={{ title: 'Name' }}
+			/>
 		</Stack.Navigator>
 	)
 }
