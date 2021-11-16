71a81a3324c2507b8446e06f94efdfe798b92389
Jordan Souchez <jsouchez@feelity.fr>
2021-11-16 21:38:42 +0100

    💄 Typo

--- core/components/StyledText.tsx
+++ core/components/StyledText.tsx
@@ -1,16 +1,34 @@
+import { useTheme } from '@react-navigation/native'
 import * as React from 'react'
 
 import { Text, TextProps } from './Themed'
 
 export function FredokaText(props: TextProps) {
-	return <Text {...props} style={[props.style, { fontFamily: 'FredokaOne' }]} />
-}
-
-export function RegularText(props: TextProps) {
+	const color = useTheme()
 	return (
 		<Text
 			{...props}
-			style={[props.style, { fontFamily: 'Montserrat-Regular' }]}
+			style={[
+				{ color: color.colors.text },
+				props.style,
+				{ fontFamily: 'FredokaOne' },
+			]}
+		/>
+	)
+}
+
+export function RegularText(props: TextProps) {
+	const color = useTheme()
+	return (
+		<Text
+			{...props}
+			style={[
+				{ color: color.colors.text },
+				props.style,
+				{
+					fontFamily: 'Montserrat-Regular',
+				},
+			]}
 		/>
 	)
 }
