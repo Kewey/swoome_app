4889b0347ba396d0ff4c0af5241b99774b589e69
Jordan Souchez <jsouchez@feelity.fr>
2021-11-24 01:50:34 +0100

    ✨ MultiStep : WIP

--- core/components/MultiStepForm.tsx
+++ core/components/MultiStepForm.tsx
@@ -1,50 +1,205 @@
-import { colorBlue, colorGrey, colorLight, colorWhite } from '@constants/Colors'
-import React from 'react'
-import { View, Text, KeyboardAvoidingView, Platform, StyleSheet, TextInput } from 'react-native'
+import React, { useRef, useState } from 'react'
+import {
+	colorBlue,
+	colorDarkBlue,
+	colorDarkerBlue,
+	colorDarkGrey,
+	colorGrey,
+	colorWhite,
+} from '@constants/Colors'
+import {
+	View,
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	TextInput,
+	ScrollView,
+	TextInputProps,
+} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Button from './Button'
 import { FredokaText, RegularText } from './StyledText'
+import { ChevronLeftIcon } from 'react-native-heroicons/outline'
+import { useTheme } from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '@types/RootTab'
+import { Controller, useForm } from 'react-hook-form'
 
-const MultiStepForm = () => {
-  return (
-    <KeyboardAvoidingView
-    behavior={Platform.OS === "ios" ? "padding" : "height"}
-    style={styles.container}
-  >
-    <SafeAreaView style={styles.container}>
-      <View style={{flex: 1}}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: 30}}>
-          <Button>dzadazd</Button>
-          <Button>Déjà un compte ?</Button>
-        </View>
-        
-        <View style={{height: 5, width: 150, backgroundColor: colorLight, borderRadius: 3, marginBottom: 40, alignSelf: 'center'}}>
-          <View style={{height: '100%', width: '50%', backgroundColor: colorBlue, borderRadius: 3}} />
-        </View>
+type Steps = {
+	steps: Step[]
+	navigation: NativeStackScreenProps<RootStackParamList, 'Root'>
+}
 
-        <View style={{maxWidth: '90%', alignSelf: 'center'}}>
-          <FredokaText style={{fontSize: 28, textAlign: 'center', marginBottom: 16}}>Comment tes amis t'appellent-ils ?</FredokaText>
-          <RegularText style={{fontSize: 15, textAlign: 'center'}}>Connaitre ton prénom va nous permettre de personnaliser ton expérience.</RegularText>
-        </View>
-      </View>
+type Action = {
+	action: () => void
+	text: string
+}
 
-      <View>
-        <View style={{borderRadius: 10, borderColor: '#eaeaea', borderWidth: 1, marginBottom: 20}}>
-          <TextInput style={{padding: 22}} placeholder={'Ex. Bob'} />
-        </View>
-        <Button block>Continuer</Button>
-      </View>
-    </SafeAreaView>
-  </KeyboardAvoidingView>
-  )
+type Step = {
+	title: string
+	content: string
+	inputName: string
+	data: TextInputProps
+	actions: Action[]
+}
+
+const MultiStepForm = ({ steps, navigation }: Steps) => {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
+	const [currentStep, setCurrentStep] = useState(0)
+
+	return (
+		<KeyboardAvoidingView
+			style={styles.container}
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+		>
+			<SafeAreaView style={{ flex: 1 }}>
+				<View style={{ marginBottom: 40 }}>
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+							marginBottom: 30,
+							marginTop: 10,
+						}}
+					>
+						<Button
+							circle
+							size='small'
+							variant={'white'}
+							onPress={() => {
+								if (currentStep === 0) {
+									navigation.goBack()
+									return
+								}
+								setCurrentStep(currentStep - 1)
+							}}
+							before={
+								<ChevronLeftIcon
+									height={14}
+									width={14}
+									stroke={colorDarkGrey}
+								/>
+							}
+						></Button>
+						<Button text textStyle={{ color: colorDarkerBlue }}>
+							Déjà un compte ?
+						</Button>
+					</View>
+
+					<View
+						style={{
+							height: 5,
+							width: 150,
+							backgroundColor: colorGrey,
+							borderRadius: 3,
+							marginBottom: 40,
+							alignSelf: 'center',
+						}}
+					>
+						<View
+							style={{
+								height: '100%',
+								width: ((currentStep + 1) * 100) / steps.length + '%',
+								backgroundColor: colorBlue,
+								borderRadius: 3,
+							}}
+						/>
+					</View>
+				</View>
+
+				{steps?.map(
+					(step, index) =>
+						index === currentStep && (
+							<>
+								<ScrollView key={index} style={{ flex: 1 }}>
+									<FredokaText
+										style={{
+											fontSize: 28,
+											textAlign: 'center',
+											marginBottom: 16,
+											color: colorDarkBlue,
+										}}
+									>
+										{step.title}
+									</FredokaText>
+									<RegularText
+										style={{
+											fontSize: 13,
+											lineHeight: 20,
+											opacity: 0.5,
+											textAlign: 'center',
+											color: colorDarkBlue,
+										}}
+									>
+										{step.content}
+									</RegularText>
+								</ScrollView>
+
+								<View style={{ marginBottom: 30 }}>
+									<View
+										style={{
+											borderRadius: 10,
+											borderColor: '#eaeaea',
+											borderWidth: 1,
+											marginBottom: 20,
+										}}
+									>
+										<Controller
+											name={step.inputName}
+											control={control}
+											render={({ field: { onChange, onBlur, value } }) => (
+												<TextInput
+													onBlur={onBlur}
+													onChangeText={onChange}
+													value={value}
+													style={{ paddingHorizontal: 22, paddingVertical: 20 }}
+													placeholder={'Ex. Bob'}
+												/>
+											)}
+										/>
+									</View>
+									<Button
+										block
+										size='large'
+										onPress={
+											() => {
+												setCurrentStep(currentStep + 1)
+											}
+											/*step.actions[0].action()*/
+										}
+									>
+										{step.actions[0].text}
+									</Button>
+									{step.actions[1] && (
+										<Button
+											text
+											block
+											textStyle={{ color: colorDarkBlue }}
+											onPress={() => step.actions[1].action}
+										>
+											{step.actions[1].text}
+										</Button>
+									)}
+								</View>
+							</>
+						)
+				)}
+			</SafeAreaView>
+		</KeyboardAvoidingView>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    alignContent: 'space-between'
-  },
+	container: {
+		flex: 1,
+		paddingHorizontal: 25,
+		backgroundColor: colorWhite,
+	},
 })
 
 export default MultiStepForm
--- screens/auth/inscription/NameScreen.tsx
+++ screens/auth/inscription/NameScreen.tsx
@@ -1,10 +1,53 @@
 import MultiStepForm from '@components/MultiStepForm'
-import { FredokaText } from '@components/StyledText'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '@types/RootTab'
 import React from 'react'
-import { View, Text } from 'react-native'
 
-export default function NameScreen() {
+export default function NameScreen({
+	navigation,
+}: {
+	navigation: NativeStackScreenProps<RootStackParamList, 'Root'>
+}) {
 	return (
-		<MultiStepForm />
+		<MultiStepForm
+			steps={[
+				{
+					title: 'Comment tes amis t’appellent-ils ?',
+					content:
+						'Connaitre ton prénom va nous permettre de personnaliser ton expérience.',
+					data: {},
+					inputName: 'name',
+					actions: [{ text: 'Suivant', action: () => console.log(`click`) }],
+				},
+				{
+					title: 'Quelle est ton adresse mail ?',
+					content:
+						'Connaitre ton prénom va nous permettre de personnaliser ton expérience. ',
+					data: {},
+					inputName: 'email',
+					actions: [{ text: 'Suivant', action: () => console.log(`click`) }],
+				},
+				{
+					title: 'Choisis un mot de passe',
+					content:
+						'Connaitre ton prénom va nous permettre de personnaliser ton expérience.',
+					data: {},
+					inputName: 'password',
+					actions: [{ text: 'Suivant', action: () => console.log(`click`) }],
+				},
+				{
+					title: 'Utiliser Face ID pour se connecter',
+					content:
+						'Connaitre ton prénom va nous permettre de personnaliser ton expérience.',
+					data: {},
+					inputName: 'FaceID',
+					actions: [
+						{ text: 'Autoriser FaceID', action: () => console.log(`click`) },
+						{ text: 'Pas maintenant', action: () => {} },
+					],
+				},
+			]}
+			// navigation={navigation}
+		/>
 	)
 }
