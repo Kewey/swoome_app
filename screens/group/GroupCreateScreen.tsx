6a01402ccc1177bffc727a30e13905910e864687
Jordan <jordan-souchez@hotmail.fr>
2021-12-05 15:01:29 +0100

    ✨ Add GroupCreate

---
+++ screens/group/GroupCreateScreen.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@components/StyledText'
+import { View } from '@components/Themed'
+import React from 'react'
+
+const GroupCreateScreen = () => {
+	return (
+		<View>
+			<Text></Text>
+		</View>
+	)
+}
+
+export default GroupCreateScreen
